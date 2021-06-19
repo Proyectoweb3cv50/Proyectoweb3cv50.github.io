7bee87652d715ebc259ff1ac95d8e3fdada27ea3
Proyectoweb3cv50 <Proyectoweb3cv50@gmail.com>
2021-06-19 01:05:35 -0500

    cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyC-8fuVBQrfEV4eANmeS8wUIWgTpTSLo9M",
+    authDomain: "proyectoweb3cv50-5a018.firebaseapp.com",
+    projectId: "proyectoweb3cv50-5a018",
+    storageBucket: "proyectoweb3cv50-5a018.appspot.com",
+    messagingSenderId: "840816661911",
+    appId: "1:840816661911:web:d98a502f580aa9b11b20d2",
+    measurementId: "G-7RGGZYWS8N"
+});
