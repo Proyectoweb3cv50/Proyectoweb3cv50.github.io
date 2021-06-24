e2e9f43756eeb71b9b620bffae5c80fc994e7113
Proyectoweb3cv50 <Proyectoweb3cv50@gmail.com>
2021-06-24 00:32:48 -0500

    cambios

--- js/init.js
+++ js/init.js
@@ -3,10 +3,10 @@
 // @ts-ignore
 firebase.initializeApp({
   apiKey: "AIzaSyC-8fuVBQrfEV4eANmeS8wUIWgTpTSLo9M",
-    authDomain: "proyectoweb3cv50-5a018.firebaseapp.com",
-    projectId: "proyectoweb3cv50-5a018",
-    storageBucket: "proyectoweb3cv50-5a018.appspot.com",
-    messagingSenderId: "840816661911",
-    appId: "1:840816661911:web:d98a502f580aa9b11b20d2",
-    measurementId: "G-7RGGZYWS8N"
+  authDomain: "proyectoweb3cv50-5a018.firebaseapp.com",
+  projectId: "proyectoweb3cv50-5a018",
+  storageBucket: "proyectoweb3cv50-5a018.appspot.com",
+  messagingSenderId: "840816661911",
+  appId: "1:840816661911:web:d98a502f580aa9b11b20d2",
+  measurementId: "G-7RGGZYWS8N",
 });
